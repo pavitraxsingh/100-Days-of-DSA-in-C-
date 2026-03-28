3c677c407df57bd084fac7ee780ca4689bab42c2
Pavitra <pavitraxsingh@gmail.com>
2026-03-28 21:20:24 +0530

    Matrix Diagonal Sum.c

--- day15.c
+++ day15.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Problem: Given a matrix, calculate the sum of its primary diagonal elements. The primary diagonal consists of elements where row index equals column index.
 
 // Input:
