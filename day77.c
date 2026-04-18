9a85c1cba803ffefcabe37fb175670a22fcd92f4
Pavitra <pavitraxsingh@gmail.com>
2026-04-18 21:19:28 +0530

    Check if Graph is Connected.c

---
+++ day77.c
@@ -0,0 +1,73 @@
+// Problem Statement
+// Using BFS or DFS, check if the entire graph is connected.
+
+// Input Format
+// n m
+// edges
+
+// Output Format
+// CONNECTED
+// NOT CONNECTED
+
+// Sample Input
+// 4 2
+// 1 2
+// 3 4
+
+// Sample Output
+// NOT CONNECTED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];
+int visited[MAX];
+int n, m;
+
+void dfs(int node) {
+    visited[node] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] == 1 && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+
+    int u, v;
+
+    // Initialize adjacency matrix
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+        visited[i] = 0;
+    }
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1; // undirected graph
+    }
+
+    // Run DFS from node 1
+    dfs(1);
+
+    // Check if all nodes are visited
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            printf("NOT CONNECTED\n");
+            return 0;
+        }
+    }
+
+    printf("CONNECTED\n");
+
+    return 0;
+}
