29ff238ca77c099d0c0fd77a8b91b7b19dcbbe9d
Pavitra <pavitraxsingh@gmail.com>
2026-05-02 22:52:11 +0530

    Heap Sort Using Max-Heap.c
    Implement Heap Sort using a Max Heap to sort an array in ascending order.

--- day40.c
+++ day40.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // Implement Heap Sort using a Max Heap to sort an array in ascending order. First build a max heap, then repeatedly extract the maximum element and place it at the end of the array.
 #include <stdio.h>
 
